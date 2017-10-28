3790adbb9ad9a2ad0d8d72edaab408c2f3d1c372
Lia5 <Lia_na5@mail.ru>
2017-10-29 01:05:05 +0300

    phone-mask

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 //select-number form
 
 jQuery(function($){
-  $("#phone").mask("+38(999) 999-9999");
+  
   });
 
-
+$(".phone-mask").mask("+38(999) 999-9999");
   $('.open-phone').click(function() {
+    $('.fix-menu__item .form__wrapper').toggleClass('open');
     $('.select-number .form__wrapper').toggleClass('open');
   });
 
