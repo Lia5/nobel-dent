24517ff5b55a68c4556f01808c655ac110a15cd4
Lia5 <Lia_na5@mail.ru>
2017-11-02 22:18:21 +0200

    al-on-4 _ fix asside

--- js/script.js
+++ js/script.js
@@ -285,9 +285,60 @@ $(function() {
   var order = new Order($('#form'), false);
 });
 
-
-
 //fix service__container-nav services
+
+
+Array.prototype.slice.call(document.querySelectorAll('#aside1, #aside2 > #service__sidebar__form')).forEach(function(a) {  // селекторы блоков, которые будут фиксироваться. Может быть как один блок, так два и более
+  var b = null, P = 43;
+  window.addEventListener('scroll', Ascroll, false);
+  document.body.addEventListener('scroll', Ascroll, false);
+  function Ascroll() {
+    if (b == null) {
+      var Sa = getComputedStyle(a, ''), s = '';
+      for (var i = 0; i < Sa.length; i++) {
+        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+          s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+        }
+      }
+      b = document.createElement('div');
+      b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+      a.insertBefore(b, a.firstChild);
+      var l = a.childNodes.length;
+      for (var i = 1; i < l; i++) {
+        b.appendChild(a.childNodes[1]);
+      }
+      a.style.height = b.getBoundingClientRect().height + 'px';
+      a.style.padding = '0';
+      a.style.border = '0';
+    }
+    var Ra = a.getBoundingClientRect(),
+        R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('#article').getBoundingClientRect().bottom + 38);
+    if ((Ra.top - P) <= 0) {
+      if ((Ra.top - P) <= R) {
+        b.className = 'stop';
+        b.style.top = - R +'px';
+        b.style.left = 0;
+      } else {
+        b.className = 'sticky';
+        b.style.top = P + 'px';
+        b.style.left = Ra.left + 'px';
+      }
+    } else {
+      b.className = '';
+      b.style.top = '';
+      b.style.left = '';
+    }
+    window.addEventListener('resize', function() {
+      a.children[0].style.width = getComputedStyle(a, '').width;
+      b.style.left = (b.className == 'sticky' ? (a.getBoundingClientRect().left + 'px') : '0');
+    }, false);
+  }
+  })
+
+
+/*
+
+
 $(document).ready(function() {
   
  var navPos, winPos, navHeight, endPos;
@@ -321,6 +372,10 @@ $(document).ready(function() {
  });
  
  });
+
+*/
+
+
 
  //fix button order
 $(document).ready(function() {
